1c5ec8f3d7b4f95dbc9c5b637a6454aee8b7c7a4
Theo Garin <garintheo76@gmail.com>
2025-02-24 22:45:46 +0100

    Création d'AuthModule intégrant AuthService, AuthController, LocalStrategy et JwtStrategy

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,22 +1,27 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    PassportModule.register({ defaultStrategy: 'jwt' }),
+    PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
+        secret: configService.get<string>('JWT_SECRET'),
         signOptions: { expiresIn: '1h' },
       }),
+      inject: [ConfigService],
     }),
+    ConfigModule,
   ],
-  controllers: [],
-  providers: [],
-  exports: [JwtModule, PassportModule],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
