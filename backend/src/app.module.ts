358cbca6ea49075e648fd265c6f86a7af8af8538
Theo Garin <garintheo76@gmail.com>
2025-02-24 22:47:32 +0100

    Mise à jour d'AppModule pour importer AuthModule et activer l'authentification

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     AppConfigModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST!,
-      port: parseInt(process.env.DATABASE_PORT!, 10) || 5432,
-      username: process.env.DATABASE_USER!,
-      password: process.env.DATABASE_PASSWORD!,
-      database: process.env.DATABASE_NAME!,
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
