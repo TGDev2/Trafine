5a2e0058717e64466ba23b0a2f4ab4d6294e9cf1
Theo Garin <garintheo76@gmail.com>
2025-02-26 13:26:53 +0100

    feat(auth): add routes for Google OAuth (init & callback)

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,19 +1,46 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+  Req,
+  Res
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
+import { Response } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  /**
-   * Endpoint POST /auth/login
-   * Protégé par LocalAuthGuard pour authentifier l'utilisateur.
-   * En cas de succès, renvoie un token JWT.
-   */
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  /**
+   * Initiation de la connexion Google
+   * On utilise 'AuthGuard('google')' qui va rediriger vers la page de login Google
+   */
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req) {
+  }
+
+  /**
+   * Callback une fois que Google a validé l'utilisateur
+   */
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  async googleAuthRedirect(@Req() req, @Res() res: Response) {
+    const user = req.user;
+
+    const token = await this.authService.login(user);
+
+    return res.redirect(`http://localhost:3001?token=${token.access_token}`);
   }
 }
