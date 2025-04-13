f2525ccab0d9c9035f5f1e14daf23866968cf3a4
Theo Garin <garintheo76@gmail.com>
2025-04-13 13:55:49 +0200

    chore: add helmet middleware for enhanced security headers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
+
+  app.use(helmet());
 
   const config = new DocumentBuilder()
     .setTitle('Trafine API')
