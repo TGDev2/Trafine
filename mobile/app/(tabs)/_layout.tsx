68213dff272401378904510521192c430d4d84af
Theo Garin <garintheo76@gmail.com>
2025-05-10 15:26:16 +0200

    fix: Delete Explore

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -75,19 +75,6 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
-        options={{
-          title: "Explore",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol
-              size={28}
-              name="chevron.left.forwardslash.chevron.right"
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
         name="report-incident"
         options={{
           title: "Report",
